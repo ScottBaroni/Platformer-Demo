2f16d17b9333be3d595847ea2b796e8bc567f8e0
Scott <sbaroni2004@gmail.com>
2025-09-03 22:05:04 -0700

    Added Level, Basic Enemy, and Lethal Zones
    Added basic level layout with tilemap palette, basic enemy controller, and lethal zones like water.

--- Assets/Scripts/PhysicsBase.cs
+++ Assets/Scripts/PhysicsBase.cs
@@ -22,12 +22,10 @@ public class PhysicsBase : MonoBehaviour
     {
         Vector2 acceleration = 9.81f * Vector2.down * gravityFactor;
         velocity += acceleration * Time.fixedDeltaTime;
-
         velocity.x = desiredx;
-
-        Vector2 movement = velocity * Time.fixedDeltaTime;
-        Movement(new Vector2(movement.x, 0), true);
-        Movement(new Vector2(0, movement.y), false);
+        Vector2 move = velocity * Time.fixedDeltaTime;
+        Movement(new Vector2(move.x, 0), true);
+        Movement(new Vector2(0, move.y), false);
     }
 
     public void Movement(Vector2 move, Boolean movex)
@@ -35,45 +33,44 @@ public class PhysicsBase : MonoBehaviour
         if (move.magnitude < 0.00001f) return;
 
         RaycastHit2D[] results = new RaycastHit2D[16];
+        grounded = false;
         int cnt = GetComponent<Rigidbody2D>().Cast(move, results, move.magnitude + 0.001f);
-
-        if (cnt > 0)
-        {
-            grounded = true;
-            return;
-        }
-        else
-        {
-            grounded = false;
-        }
-        transform.position += (Vector3)move;
+        bool collision = false;
 
         for (int i = 0; i < cnt; i++)
         {
             if (Mathf.Abs(results[i].normal.x) > 0.5 && movex)
             {
-                move.x = 0;
-                velocity.x = 0;
-                CollideWithHorizontal(results[i].collider);
+                collision = true;
+                CollideHorizontal(results[i].collider);
             }
 
             if (Mathf.Abs(results[i].normal.y) > 0.5 && !movex)
             {
-                move.y = 0;
-                velocity.y = 0;
-                CollideWithVertical(results[i].collider);
+                if (results[i].normal.y > 0.3f) grounded = true;
+                collision = true;
+                CollideVertical(results[i].collider);
             }
         }
-
+        if (collision) return;
+        transform.position += (Vector3)move;
     }
 
-    public virtual void CollideWithHorizontal(Collider2D other)
+    public void Collide(Collider2D other)
     {
+        if (other.gameObject.CompareTag("Lethal"))
+        {
+            Debug.Log("RIP");
+        }
+    }
 
+    public virtual void CollideHorizontal(Collider2D other)
+    {
+        Collide(other);
     }
     
-    public virtual void CollideWithVertical(Collider2D other)
+    public virtual void CollideVertical(Collider2D other)
     {
-        
+        Collide(other);
     }
 }
