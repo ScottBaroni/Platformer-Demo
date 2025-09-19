552ff843733b2011e3c56a4f03e4f9ab2ceb3cbc
Scott <sbaroni2004@gmail.com>
2025-09-18 21:31:42 -0700

    Adjusted resolution, WebGL build
    Made WebGL build and adjusted resolution to better suit it.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject credits;
     public void StartButton()
     {
         SceneManager.LoadScene("Bookworming");
@@ -14,5 +16,13 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Game closed");
+    }
+
+    public void CreditsButton()
+    {
+        if (credits != null)
+            {
+                credits.SetActive(!credits.activeSelf);
+            }
     }
 }
